810ad315151b58c307353dcf39438b3df76ccd51
Kai Anter <kai@anter.dev>
2022-11-04 22:52:15 +0100

    Added: files for packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uniulm_mensaparser",
+    version="0.1.0",
+    author="Tanikai",
+    author_email="kai@anter.dev",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tanikai/uniulm_mensaparser",
+    project_urls={
+        "Bug Tracker": "https://github.com/Tanikai/uniulm_mensaparser/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=["mensa_parser"],
+    python_requires=">=3.0",
+    install_requires=[
+        "beautifulsoup4",
+        "pymupdf",
+        "requests"
+    ]
+)
